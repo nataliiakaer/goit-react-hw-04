f383e42045e29f83c03ab6fd9e0a550ff18cb5bf
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2024-08-10 00:19:38 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,26 @@ import { useEffect, useState } from "react";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
 import SearchBar from "./SearchBar/SearchBar";
-import axios from "axios";
 import Loader from "./Loader/Loader";
 import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import { fetchImagesWithSearch } from "../images_api";
 
 const App = () => {
   const [images, setImages] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
-  const ACCESS_KEY = "E1xxE1oKTwgg_W6JmdrRoPbDgn8gMbq3_X0T4N2uKC0";
-
   useEffect(() => {
-    const fetchImage = async () => {
-      setLoading(true);
-      setError(true);
-      const { data } = await axios.get(
-        `https://api.unsplash.com/search/photos/?client_id=${ACCESS_KEY}&per_page=12&query=office`
-      );
-      console.log(data.results);
-      setImages(data.results);
-      setLoading(false);
-    };
+    async function fetchImage() {
+      try {
+        setLoading(true);
+        const data = await fetchImagesWithSearch("office");
+        setImages(data);
+      }
+      catch (error) {setError(true);}
+      finally {
+        setLoading(false);
+      }
+    }
 
     fetchImage();
   }, []);
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,15 +2,36 @@ import css from "./SearchBar.module.css";
 import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
+    
+    const handleSubmit = (evt) => {
+      evt.preventDefault();
+      // const form = evt;
+      // console.log(form);
+      // const topic = form.elements.topic.value;
+      // console.log(topic);
+      
+      // // Якщо текстове поле порожнє, виводимо повідомлення 
+      // // і припиняємо виконання функції.
+      // if(form.elements.topic.value.trim() === "") {
+      //   alert("Please enter search term!")
+      //   return;
+      // }
+  
+      // // У протилежному випадку викликаємо пропс 
+      // // і передаємо йому значення поля
+      // onSearch(topic);
+      // form.reset();
+    };
+
   return (
     <header className={css.header}>
-      <form className={css.form}>
+      <form className={css.form} onSubmit={handleSubmit}>
         <input
         className={css.input}
-          type="text"
-          // autocomplete="off"
-          // autofocus
-          placeholder="Search images and photos"
+        type="text"
+        // autocomplete="off"
+        // autofocus
+        placeholder="Search images and photos"
         />
         <button className={css.btn} type="submit">
           <CiSearch />
